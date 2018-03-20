fbc74cc1f2075c0c3c8527fa5bc46e87d94ed60c
james-chege <jymeschege@gmail.com>
2018-03-19 22:32:30 -0700

    add form submission hundler

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -11,9 +12,21 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function index()
+    public function index(Request $request)
     {
         $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+                       $contactFormData = $form->getData();
+
+                       dump($contactFormData);
+
+                       // do something interesting here
+                   }
+
         return $this->render('contact/index.html.twig', [
                        'our_form' => $form->createView(),
                   ]);
